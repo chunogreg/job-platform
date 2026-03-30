911b96feb74332fbcd99a29b17082e29fba1ac58
chunogreg <chunogreg@yahoo.com>
2026-03-30 22:12:50 +0100

    Updated UI, title and real job data

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,43 @@ const App = () => {
 
   return (
     <>
-      <h1>Job Listing</h1>
-      {jobs.map((job, index) => (
+      <h1>🌍 Global Opportunities (Remote & Visa Sponsored Jobs)</h1>
+      <p>
+        Find global job oppotunities including - fully remote (worldwide)
+        -Onsite roles with visa sponsorship - Entry-Level positions
+      </p>
+      <h2>Job Listing</h2>
+      <div className="boxes">
+        <label>
+          <input
+            type="checkbox"
+            checked={remoteWorldwide}
+            onChange={(e) => setRemoteWorldwide(e.target.checked)}
+          />
+          Remote Worldwide
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            checked={visaAllow}
+            onChange={(e) => setVisaAllow(e.target.checked)}
+          />
+          Visa Sponsorship
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={entryLevel}
+            onChange={(e) => setEntryLevel(e.target.checked)}
+          />
+          Entry Level / Junior
+        </label>
+      </div>
+
+      <br />
+
+      {filteredJobs.map((job, index) => (
         <div key={index}>
           <h3> {job.title}</h3>
           <p> {job.company}</p>
@@ -25,47 +60,21 @@ const App = () => {
           </p>
           <p> {job.visa ? "Visa Sponsorship" : "❌ Visa Sponsorship"}</p>
           <p> Level: {job.level}</p>
-          <p> Link: {job.link}</p>
+
+          <p>
+            <a href={job.link} target="_blank" rel="noopener noreferrer">
+              Apply Now
+            </a>
+          </p>
         </div>
       ))}
-      <label>
-        <input
-          type="checkbox"
-          checked={remoteWorldwide}
-          onChange={(e) => setRemoteWorldwide(e.target.checked)}
-        />
-        Remote Worldwide
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          checked={visaAllow}
-          onChange={(e) => setVisaAllow(e.target.checked)}
-        />
-        Visa Sponsorship
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={entryLevel}
-          onChange={(e) => setEntryLevel(e.target.checked)}
-        />
-        Entry Level / Junior
-      </label>
-      {filteredJobs.map((job, index) => (
-        <div key={index}>
-          <h3> {job.title}</h3>
-          <p> {job.company}</p>
-          <p>Remote: {job.remote}</p>
-          <p> Visa: {job.visa ? "Yes" : "No"}</p>
-          <p> Level: {job.level}</p>
-        </div>
-      ))}
-      <br />
-      <button onClick={() => window.open("https://google.com")}>Apply</button>
-      <br />
-      <br />
+      <div>
+        <p>Last updated: March 2026</p>
+        <p>
+          Jobs are sourced from public listings and link directly to original
+          application pages
+        </p>
+      </div>
     </>
   );
 };
